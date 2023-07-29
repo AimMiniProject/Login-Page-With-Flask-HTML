06bba0497170d6db24291d4eca06312995ee9d0f
AimMiniProject <133402879+AimMiniProject@users.noreply.github.com>
2023-07-29 19:58:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("login.html")
+    return render_template("index.html")
 database={'nachi':'123','james':'aac','karthik':'asdsf'}
 
 @app.route('/form_login',methods=['POST','GET'])
@@ -14,10 +14,10 @@ def login():
     name1=request.form['username']
     pwd=request.form['password']
     if name1 not in database:
-	    return render_template('login.html',info='Invalid User')
+	    return render_template('index.html',info='Invalid User')
     else:
         if database[name1]!=pwd:
-            return render_template('login.html',info='Invalid Password')
+            return render_template('index.html',info='Invalid Password')
         else:
 	         return render_template('home.html',name=name1)
 
